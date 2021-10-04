f36a8396a422cfd3aa2c6bf2be5abd4324ba17c9
Savo Lazic <savo.lazic@oicr.on.ca>
2021-10-04 18:29:37 -0400

    Add packages to setup.py to fix failing build.

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     description="Simplified interface to the samtools sub-sampling algorithm.",
     author="Savo Lazic",
     author_email="savo.lazic@oicr.on.ca",
+    packages=["simple_downsample"],
     entry_points={"console_scripts": ["simple-downsample = simple_downsample.main:main"]},
 )
