e51639d1e7c27b5040c5c5cadfda0ef5c380a4fb
Savo Lazic <savo.lazic@oicr.on.ca>
2021-10-04 16:41:01 -0400

    Changed bin name in the hope of fixing weird Modulator error.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="simple_downsample",
+    name="simple-downsample",
     version="0.1",
     description="Simplified interface to the samtools sub-sampling algorithm.",
     author="Savo Lazic",
     author_email="savo.lazic@oicr.on.ca",
-    entry_points={"console_scripts": ["simple_downsample = simple_downsample.main:main"]},
+    entry_points={"console_scripts": ["simple-downsample = simple_downsample.main:main"]},
 )
